489f946ec5d1c1a4f8b13be39b612018c7b69225
Griffin Pilz <griffinpilz@Griffins-Mac-mini.local>
2017-10-09 20:20:49 -0400

    end of day october 9

--- src/components/pageLogin/pageLogin.js
+++ src/components/pageLogin/pageLogin.js
@@ -16,7 +16,6 @@ class pageLoginModel {
         this.context = context;
         this.id = this.context.util.guid();
         this.visible = context.showlogin;
-        this.ticketarray = ko.observableArray([]);
 
         this.password = ko.observable("");
         this.username = ko.observable("");
@@ -24,24 +23,12 @@ class pageLoginModel {
         this.incorrectusername = ko.observable(false);
         this.loginmodalclass = ko.observable("");
 
-        this.getAuthData();
-
     }
 
 
     handleRegistration() {
         this.context.showregistration(true);
         this.visible(false);
-    }
-
-    handleTicketPage() {
-        this.visible(false);
-        this.context.showticketpage(true);
-    }
-
-    handleQueryPage() {
-        this.visible(false);
-        this.context.showquerypage(true);
     }
 
     checkUserName() {
@@ -54,26 +41,63 @@ class pageLoginModel {
             this.context.showticketpage(true);
         } else {
             this.incorrectusername(true);
-            $('.flashThis').animateCss('bounceIn');;
+            $('.flashThis').animateCss('bounceIn');
         }
         }
 
     getAuthData() {
 
-        this.ticketarray([]);
-
         let url = context.apiUrl + '/auth';
 
         axios({
             url: url,
-            method: 'get',
-            headers: this.context.apiType
+            method: 'post',
+            headers: this.context.apiType,
+            data: {"email": this.username(), "Password": this.password()}
         }).then((response) => {
             this.handleGetData(response.data);
         }).catch((error) =>{
             console.log("customerTable: getData Error");
             console.log(error);
         });
+    }
+
+    handleGetData(data) {
+
+        if (typeof data.status !== undefined) {
+            console.log(data);
+            this.handleCheckValidity(data);
+        } else {
+            this.incorrectusername(true);
+            $('.flashThis').animateCss('bounceIn');
+            console.log("undefined");
+        }
+
+
+    }
+
+    handleCheckValidity(data) {
+        if (data.status === "Invalid") {
+            this.incorrectusername(true);
+            $('.flashThis').animateCss('bounceIn');
+            console.log("invalid");
+        } else {
+            console.log(data);
+            this.handleCheckAdmin(data[0]);
+        }
+    }
+
+    handleCheckAdmin(data) {
+        if (data.Admin === 1){
+            this.visible(false);
+            this.context.showquerypage(true);
+            console.log("Admin");
+        } else {
+            this.visible(false);
+            this.context.showticketpage(true);
+            console.log("Not an Admin");
+            console.log(data)
+        }
     }
 
     }
